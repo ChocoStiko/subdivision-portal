42f0ff21d2ca9ecf08747d6f23f17174ed8ebee5
ChocoStiko <ejangcanan07@gmail.com>
2025-10-03 00:23:32 +0800

    add navbar component

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,54 @@
+import logo from "../assets/wwhs_logo.png";
+import userIcon from "../assets/user.svg";
+import "../styles.css";
+
+
+function NavbarComponent(){
+    return(
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container-fluid">
+            <a className="navbar-brand me-auto" href="index.html">
+            <img src={logo} alt="Logo" width="70" height="70" className="d-inline-block align-text-top"/>
+            </a>
+            
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top"/>
+                </h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+                <li className="nav-item">
+                    <a className="nav-link mx-lg-2 active" aria-current="page" href="index.html">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="pages/about_us_page.html">About Us</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle mx-lg-2 active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Offices</a>
+                    <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="pages/hoa_page.html">HOA</a></li>
+                    <li><a className="dropdown-item" href="pages/health_center_page.html">Health Center</a></li>
+                    <li>
+                        <hr className="dropdown-divider"/>
+                    </li>
+                    <li><a className="dropdown-item" href="pages/senior_citizen_page.html">Senior Citizens</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
+            </div>
+            <a href="pages/login_page.html" className="user-button">
+            <img src={userIcon} alt="User Icon" width="40" height="40" className="d-inline-block align-text-top"/>
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        </nav>
+    );
+}
+
+export default NavbarComponent ;
