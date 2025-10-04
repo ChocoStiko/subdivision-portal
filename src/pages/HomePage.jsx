81d9810580a3227bc674b21ffee5dbbb879e0f8a
ChocoStiko <ejangcanan07@gmail.com>
2025-10-05 00:19:45 +0800

    add content in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,11 @@ import "../styles.css";
 import filinvestLogo from "../assets/filinvest logo.png";
 import wwhsLogo from "../assets/wwhs_logo.png"
 import subdImg from "../assets/sample subd.jpg";
+import clubhouseImg from "../assets/clubhouse.jpg";
+import courtImg from "../assets/court.jpg";
+import poolImg from "../assets/pool.jpg"
+import aerialImg from "../assets/subd-aerial.jpg";
+import parkImg from "../assets/park.jpg";
 
 function HomePage() {
 
@@ -11,97 +16,128 @@ function HomePage() {
         <HeroComponent/>
 
         {/* Filinvest Section */}
-        <section className="brands">
-            <div className="row">
-                <div className="col">
-                    <img src={filinvestLogo} alt="Filinvest Logo"/>
-                </div>
-                <div className="col">
-                    <img src={wwhsLogo} alt="Filinvest Logo"/>
-                </div>
-                    <div className="col">
-                        <img src={filinvestLogo} alt="Filinvest Logo"/>
-                    </div>
-                <div className="col">
-                    <img src={wwhsLogo} alt="Filinvest Logo"/>
-                </div>
+        <section class="brands">
+        <div class="row">
+            <div class="col">
+            <img src={filinvestLogo} alt="Filinvest Logo"/>
             </div>
+            <div class="col">
+            <img src={wwhsLogo} alt="Filinvest Logo"/>
+            </div>
+            <div class="col">
+            <img src={filinvestLogo} alt="Filinvest Logo"/>
+            </div>
+            <div class="col">
+            <img src={wwhsLogo} alt="Filinvest Logo"/>
+            </div>
+        </div>
         </section>
         
         {/*About Us Section*/}
-        <section className="about-us" id="about-us">
-            <div className="container">
-                <img src= {subdImg} className="subd-img rounded mx-auto d-block" alt="subd"/>
-                <h2>About Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <section class="about-us" id="about-us">
+        <div class="container">
+            <div class="about-content">
+            <div class="subd-aerial-img">
+                <img src={aerialImg} alt="subd-aerial"/>
             </div>
+            <div class="about-text">
+                <h2>About Us</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            </div>
+        </div>
         </section>
 
         {/*about us discover section*/}
         <section class="discover-sect">
-            <div class="row">
-                <div class="col">
-                    <div class="card text-bg-dark">
-                        <img src="..." class="card-img" alt="..."/>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
+        <div class="container">
+            <div class="row g-3 no-wrap-row">
+
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                <img src={clubhouseImg} class="card-img" alt="WWHS Clubhouse"/>
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Clubhouse</h5>
+                    <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
                 </div>
             </div>
 
-            <div class="col">
+            <div class="col-3">
                 <div class="card text-bg-dark">
-                    <img src="..." class="card-img" alt="..."/>
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                    </div>
+                <img src={poolImg} class="card-img" alt="WWHS Pool"/>
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Community Pool</h5>
+                    <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
                 </div>
             </div>
 
-            <div class="col">
+            <div class="col-3">
                 <div class="card text-bg-dark">
-                    <img src="..." class="card-img" alt="..."/>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                        </div>
+                <img src={courtImg} class="card-img" alt="WWHS Court"/>
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Basketball Court</h5>
+                    <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
                 </div>
             </div>
 
-            <div class="col">
+            <div class="col-3">
                 <div class="card text-bg-dark">
-                <img src="..." class="card-img" alt="..."/>
-                    <div class="card-img-overlay">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                    </div>
+                <img src={parkImg} class="card-img" alt="WWHS Park"/>
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Park</h5>
+                    <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
                 </div>
             </div>
+
+            </div>
+        </div>
         </section>
 
         {/*About Us Why Choose Us*/}
-        <section class="abt-us-choose">
-            <div class="about-us choose">
-                <h2>Why Choose Us!</h2>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <section class="about-us-choose">
+            <h2 class="title">Why Choose Us?</h2>
+
+            <div class="grid-box">
+            <div class="square">
+            <span class="num">01</span>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a, quo labore quis nam modi?</p>
             </div>
+
+
+            <div class="square">
+            <span class="num">02</span>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a, quo labore quis nam modi?</p>
+            </div>
+
+            <div class="square">
+            <span class="num">03</span>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a, quo labore quis nam modi?</p>
+            </div>
+
+            <div class="square">
+            <span class="num">04</span>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a, quo labore quis nam modi?</p>
+            </div>
+        </div>
         </section>
 
         {/*get in touch section*/}
         <section class="get-in-touch">
-            <div class="get-in-touch">
-                <h2>Get in Touch</h2>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+        <div class="get-in-touch">
+            <h2>Get in Touch</h2>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
         </section>
-
         </>
     );
     
