fab18f3d815eb3794f3a1f17737f789b86f1e476
ChocoStiko <ejangcanan07@gmail.com>
2025-10-10 22:24:36 +0800

    fix responsiveness for clubhouse, court, and pool page

--- src/pages/reserve/CourtPage.jsx
+++ src/pages/reserve/CourtPage.jsx
@@ -3,6 +3,7 @@ import UserNavbarComponent from "../../components/UserNavbarComponent";
 import styles from "../../css/servicestyles.module.css";
 import courtImg from "../../assets/court_reservation_img.jpg";
 import PopupReserveComponent from "../../components/PopupReserveComponent";
+import { Container, Row, Col } from "react-bootstrap";
 
 
 
@@ -12,35 +13,27 @@ function CourtPage(){
     return(
     <>
     <UserNavbarComponent/>
-        <div className={styles.clubhouse_section}>
-            <div className={`container py-5 ${styles.reservation_section}`}>
-            <div id={styles.container}>
-            <div className="row align-items-center">
-
-    
-                <div className="col-md-6">
-                <h2 className={`fw-bold text-success ${styles.text_title}`}>Lorem ipsum dolor sit</h2>
-                <p className="text-dark">
+    <div className={styles.reservation_type_section}>
+        <Container fluid className={`container ${styles.reservation_container}`}>
+            <Row className="align-items-center">
+                <Col xs={{order : 1}} md={{order : 2}} className={styles.reservation_text}>
+                    <h2 className={`fw-bold text-success ${styles.text_title}`}>Lorem ipsum dolor sit</h2>
+                    <p className="text-dark">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ornare blandit.
                     Aliquam bibendum diam leo, sit amet luctus tellus consectetur at. Aenean mollis fringilla justo eget malesuada.
-                </p>
-                <p className="text-dark">
-                    Curabitur ac erat ut magna tempor faucibus id et nisi. Nam imperdiet ornare blandit.
-                </p>
-                <PopupReserveComponent/>            
-                </div>
-
-                <div className="col-md-6 position-relative text-center">
-                <img src={courtImg} className="img-fluid rounded shadow" alt="Clubhouse" />
-                </div>
-
-            </div>   
-            </div>
-            </div>
-        </div>
-        
+                    </p>
+                    <div className={styles.btn_area}>
+                        <PopupReserveComponent/> 
+                    </div>   
+                </Col>
+                <Col xs={12} md={6} className="position-relative text-center">
+                    <img src={courtImg} className="img-fluid rounded shadow" alt="Clubhouse" />
+                </Col>
+            </Row>
+        </Container>
+    </div>
     <FooterComponent/>
-    </>      
+    </>  
     )
 
 }
