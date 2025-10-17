57673d45990d58b6a1fa28eaf27770f7950687c7
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2025-10-18 00:06:50 +0800

    push ko lng d pa tapo ui (ROAD ADVISORIES)

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -1,6 +1,9 @@
 import FooterComponent from '../components/FooterComponent';
 import UserNavbarComponent from '../components/UserNavbarComponent';
 import style from '../css/userhomestyles.module.css';
+import React from 'react';
+import drive1 from '../assets/clubhouse.jpg'
+import { Container, Row, Col } from 'react-bootstrap'
 
 
 
@@ -9,12 +12,65 @@ function UserHomePage(){
         <>
         <UserNavbarComponent/>
       
+      {/*Hero*/}
         <section className={style.hero_user}>
-            <div className={style.hero_content}>
-                <h1>Welcome to the WWHS Portal</h1>
-            </div>
-        </section>
-        <FooterComponent/>
+                <div className={style.hero_content}>
+                    <h1>Welcome to the WWHS Portal</h1>
+                </div>
+            </section>
+
+            <section className={style.road_advisories}>
+        <Container>
+          <h2 className={style.section_title}>Road Advisories</h2>
+          <Row className="g-4">  
+            <Col xs={12} md={6} lg={3}> 
+              <div className="card" style={{ width: '100%' }}> 
+                <img src={drive1} alt="Subd Aerial" className="card-img-top" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card’s content.
+                  </p>
+                </div>
+              </div>
+            </Col>
+
+            <Col xs={12} md={6} lg={3}>
+              <div className="card" style={{ width: '100%' }}>
+                <img src={drive1} alt="Subd Aerial" className="card-img-top" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card’s content.
+                  </p>
+                </div>
+              </div>
+            </Col>
+
+            <Col xs={12} md={6} lg={3}>
+              <div className="card" style={{ width: '100%' }}>
+                <img src={drive1} alt="Subd Aerial" className="card-img-top" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card’s content.
+                  </p>
+                </div>
+              </div>
+            </Col>
+
+            <Col xs={12} md={6} lg={3}>
+              <div className="card" style={{ width: '100%' }}>
+                <img src={drive1} alt="Subd Aerial" className="card-img-top" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card’s content.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+            <FooterComponent />
         </>
     )
 }
